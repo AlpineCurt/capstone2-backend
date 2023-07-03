2b5e6e9099a46ab2932dec3f10eeb251db381c3c
Curt <alpinecurt@gmail.com>
2023-07-03 12:08:04 -0700

    cleaned up documents

--- models/Game.test.js
+++
@@ -1,4 +0,0 @@
-test("something here", async () => {
-    //const resp = await request(app).get("/no-such-path");
-    expect(1).toEqual(1);
-});
--- websocket.test.js
+++ websocket.test.js
@@ -1,3 +1,7 @@
+/** This test is imcomplete and not working correctly.
+ * This is a work in progress.
+ */
+
 const { startServer, waitForSocketState } = require("./webSocketTestUtils");
 const WebSocket = require("ws");
 const app = require("./app");
@@ -6,25 +10,15 @@ const port = 3001;
 
 describe("WebSocket Server", () => {
     let server;
-    //let client;
 
     beforeAll(async () => {
         server = await startServer(port);
-        //server = app.listen(port);
 
     })
 
     afterAll(() => {
-        //console.log("SERVER: ", server);
         server.close()
-        // Says .close is not a method, yet server is a returned
-        // express app.listen.  WHY?
     });
-
-    // beforeEach(async () => {
-    //     client = new WebSocket(`ws://localhost:${port}/games/aaaaa`);
-    //     await waitForSocketState(client, client.OPEN);
-    // })
 
     test("can open WS connection", async () => {
         // Create test client
@@ -37,11 +31,9 @@ describe("WebSocket Server", () => {
         client.on("message", (data) => {
             responseMessage = data;
             console.log("RESPONSE MESSAGE: ", responseMessage);
-            // responseMessage comes in as Hexadecimal.  WHY?
 
             //Close the client after it receives the response
             client.close();
-            // I think this is not working because see below...
         });
 
         // Send client message
@@ -49,7 +41,6 @@ describe("WebSocket Server", () => {
 
         // Perform Assertions on the response
         await waitForSocketState(client, client.CLOSED);
-        // I think this isn't resolving becase the console.log below never prints
         console.log("responseMessage: ", responseMessage);
     });
 });
